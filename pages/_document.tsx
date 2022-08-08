5b1cc5b2c569127c983c595871a329a98473814d
babono <muhammad.akbar@gmail.com>
2022-08-08 21:01:50 +0700

    disable zoom

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,7 @@ export default class MyDocument extends Document {
 		  `,
 			}}
 		  />
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+			<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		</Head>
 		<body>
 		  <Main />
