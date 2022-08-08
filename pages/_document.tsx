6ce79a02a8699cdf03a7d248f3ec3e64fb2d4dc4
babono <muhammad.akbar@gmail.com>
2022-08-08 21:19:22 +0700

    update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,6 @@ export default class MyDocument extends Document {
 		  `,
 			}}
 		  />
-			<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		</Head>
 		<body>
 		  <Main />
