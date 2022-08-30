5a7d5366bad6a86d6b9230c40bb3f80d60bf122b
babono <muhammad.akbar@gmail.com>
2022-08-30 22:04:04 +0700

    add script adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
@@ -8,7 +9,7 @@ import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
-  
+
     useEffect(() => {
       const handleRouteChange = (url:string) => {
         ga.pageview(url)
@@ -16,14 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       //When the component is mounted, subscribe to router changes
       //and log those page views
       router.events.on('routeChangeComplete', handleRouteChange)
-  
+
       // If the component is unmounted, unsubscribe
       // from the event with the `off` method
       return () => {
         router.events.off('routeChangeComplete', handleRouteChange)
       }
     }, [router.events])
-  
-    return <Component {...pageProps} />
+
+    return (<><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2466930201417951" /><Component {...pageProps} /></>)
 }
 export default MyApp
